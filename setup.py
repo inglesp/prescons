3f36930c4ff9ccb0d4d3079e6b8fa776753e521d
Peter Inglesby <peter.inglesby@gmail.com>
2013-08-24 13:22:02 +0100

    Fix ImportError for Python 3
    Refs #5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='prescons',
-    version='0.2',
+    version='0.2.1',
     description='Presentation Console',
     long_description=open('README').read(),
     packages=['prescons'],
